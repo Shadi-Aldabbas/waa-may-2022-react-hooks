69d6d763f4d89672c79db676d8debd804e04b35a
shadidabbasoptimum <shadi.dabbas@optimumpartners.co>
2022-05-11 11:54:55 -0500

    be

---
+++ src/Product.js
@@ -0,0 +1,12 @@
+
+
+function Product (props){
+        return (
+            <div>
+                    <h1>{props.name}</h1>
+                    <h4>{props.price}</h4>
+                </div>
+        )
+};
+
+export default Product;
